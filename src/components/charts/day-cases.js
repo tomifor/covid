01bb7b6f8138c113907b45cfd40e64bae8d68123
Tomas <tomas_forman@outlook.com>
2020-05-30 00:59:05 -0300

    change font family to oxygen

--- src/components/charts/day-cases.js
+++ src/components/charts/day-cases.js
@@ -8,7 +8,9 @@ export default class DayCases extends React.Component {
     constructor(props) {
         super(props);
         const values = DATA.map(item => ({x: new Date(Date.parse(item.date)).getTime(), y: item.cases.newCases})).reverse();
-        this.state = {data: values}
+        const maxValue = Math.max.apply(Math, values.map((o) => { return o.y; }));
+        console.log(maxValue);
+        this.state = {data: values, max: maxValue + 2}
     }
 
     render() {
@@ -17,19 +19,21 @@ export default class DayCases extends React.Component {
                 <h3 className={'chart-title'}>Casos por día</h3>
                 <VictoryChart height={300}
                               domainPadding={{ x: 50, y: [0, 5] }}
-                              padding={{top: 10, bottom: 40, right: 10, left: 35}}
+                              domain={{y: [0, this.state.max]}}
+                              padding={{top: 20, bottom: 40, right: 10, left: 35}}
                               scale={{ x: "time" }}>
                     <VictoryBar
                         style={{ data: { fill: "#ff932a" } }}
                         data={this.state.data}
                         scale={{x: 'time', y: 'linear'}}
                         minDomain={{y: 0}}
+                        labels={({ datum }) => datum.y > 4 ? datum.y : ''}
                         animate={{
                             duration: 2000,
                             onLoad: {duration: 1000}
                         }}
                     />
-                    <VictoryAxis dependentAxis />
+                    <VictoryAxis dependentAxis tickCount={6} />
                     <VictoryAxis tickCount={3} tickFormat={v => month[new Date(v).getMonth()]}/>
                 </VictoryChart>
             </div>
