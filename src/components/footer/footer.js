847d209af63ebfdd99f43bf91d3d72ce2e6db4fd
Tomas <tomas_forman@outlook.com>
2020-06-17 18:54:31 -0300

    update version. deploy

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,7 +7,7 @@ export default class Footer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            version: 'v 0.5.1',
+            version: 'v 0.5.2',
             tag: '#QuedateEnCasa',
             twitterUrl: 'https://twitter.com/hashtag/QuedateEnCasa',
             githubUrl: 'https://github.com/tomifor/covid',
