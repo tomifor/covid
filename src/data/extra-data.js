2944e72d7b37a61b72cd3049a06e518f0b7de6d4
Tomas <tomas_forman@outlook.com>
2020-06-24 10:37:49 -0300

    update extra data

--- src/data/extra-data.js
+++ src/data/extra-data.js
@@ -1,11 +1,11 @@
 // Actualizado 05/06/20
 export const Lethality = {
-    argentina: 2.47,
-    caba: 2.18,
-    buenosaires: 2.53,
+    argentina: 2.28,
+    caba: 2.08,
+    buenosaires: 2.13,
 }
 
 export const ArgentinaData = {
     avgAgeDeceased: 74,
-    intensiveCarePatients: 364
+    intensiveCarePatients: 414
 }
