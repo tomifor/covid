8a88ef71a6b151a93414eae1056395c7ebca2ad2
Tomas <tomas_forman@outlook.com>
2020-07-10 23:03:05 -0300

    add mail to footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './footer.scss';
 import styled from "@emotion/styled";
+import MailIcon from '../../assets/correo.svg';
 
 export default class Footer extends React.Component {
 
@@ -50,6 +51,13 @@ export default class Footer extends React.Component {
                                 <path
                                     d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z"/>
                             </svg>
+                        </a>
+                        <a className={'icon mail'}
+                           href={'mailto:tomas_forman@outlook.com'}
+                           name={'mail'}
+                           role={'button'}
+                           aria-label={'mail'}>
+                            <img src={MailIcon} alt={'Mail'}/>
                         </a>
                     </div>
                     <p className={'made-by'}>Made by <a href={'https://twitter.com/tomifor'} rel={'noopener noreferrer'}
